3704a207ddc2d4d1c84a007abf5257e87f6c6947
mohamedelbadawi <redamohamed619@gmail.com>
2022-07-21 12:57:50 +0100

    update categories seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,20 +18,20 @@ class CategorySeeder extends Seeder
         Category::create(['name' => [
             'en' => 'Vegetables',
             'ar' => 'الخضراوات'
-        ], 'image' => 'assets/categories/vegetables.png', 'created_at' => now()]);
+        ], 'image' => 'assets/categories/Vegetables_1658327344_.jpg', 'created_at' => now()]);
         Category::create(['name' => [
             'en' => 'Fruits',
             'ar' => 'الفواكة'
 
-        ], 'image' => 'assets/categories/fruits.png', 'created_at' => now()]);
+        ], 'image' => 'assets/categories/Fruits_1658327326_.jpg', 'created_at' => now()]);
         Category::create(['name' => [
             'en' => 'Meat and chicken',
             'ar' => 'اللحوم والدواجن'
-        ], 'image' => 'assets/categories/meat.png', 'created_at' => now()]);
+        ], 'image' => 'assets/categories/Meatandchicken_1658329083_.jpg', 'created_at' => now()]);
         Category::create(['name' => [
             'en' => 'Diary',
             'ar' => 'منتجات الالبان'
 
-        ], 'image' => 'assets/categories/diary.png', 'created_at' => now()]);
+        ], 'image' => 'assets/categories/Diary_1658327588_.jpg', 'created_at' => now()]);
     }
 }
