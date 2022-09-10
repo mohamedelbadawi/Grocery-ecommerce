c1020c6857f58a356de2277c57511f469f19e796
mohamedelbadawi <redamohamed619@gmail.com>
2022-09-10 10:57:26 +0100

    update data

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -23,7 +23,7 @@ class ProductResource extends JsonResource
             'stock' => $this->stock,
             'category' => Category::findOrFail($this->category_id)->name,
             'status' => $this->status,
-            'rate' => $this->totalRate(),
+            'rate' => $this->rate,
             'images' => ImageResource::collection($this->images)
 
         ];
